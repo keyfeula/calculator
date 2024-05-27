8b046dcc80c0403065adc435c49d27fe8e68cb2c
Key Feula <keyfeula1214@gmail.com>
2024-05-27 13:11:17 -0700

    Add variables for operands and operator and write operate function
    Need variables to store the operands and operator for a math expression and function to handle calling the
    correct function given an operator

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let operand1 = 0;
+let operand2 = 0;
+let operator = "";
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +16,25 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(operand1, operand2, operator) {
+    let result = 0;
+    switch (operator) {
+        case "+":
+            result = add(operand1, operand2);
+            break;
+        case "-":
+            result = subtract(operand1, operand2);
+            break;
+        case "*":
+            result = multiply(operand1, operand2);
+            break;
+        case "/":
+            result = divide(operand1, operand2);
+            break;
+        default:
+            result = 0;
+    }
+    return result;
 }
