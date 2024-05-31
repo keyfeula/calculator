be1ebe4ef0cb3d220df7324fb6653203134b76e6
Key Feula <keyfeula1214@gmail.com>
2024-05-31 14:58:39 -0700

    Add img folder for tab icon, refactor calculator to handle negative numbers and decimals, as well as handle division by 0

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let result = 0;
-let expression = ["", "", ""];
+let operand1 = "";
+let operand2 = "";
+let operator = "";
+let operatorPressed = false;
+let currentOperand = "operand1";
 
 const calcBody = document.querySelector(".calc-body");
 const currentDisplay = document.querySelector(".current-display");
@@ -25,6 +29,7 @@ function operate(operand1, operand2, operator) {
     let result = 0;
     operand1 = Number(operand1);
     operand2 = Number(operand2);
+
     switch (operator) {
         case "+":
             result = add(operand1, operand2);
@@ -36,12 +41,63 @@ function operate(operand1, operand2, operator) {
             result = multiply(operand1, operand2);
             break;
         case "/":
+            if (operand2 === 0) {
+                return "divide by 0";
+            }
             result = divide(operand1, operand2);
             break;
         default:
             result = 0;
     }
+    if (!Number.isInteger(result)) {
+        result = result.toFixed(2);
+    }
     return result;
+}
+
+function clearCalculator() {
+    operand1 = "";
+    operand2 = "";
+    operator = "";
+    result = "";
+    operatorPressed = false;
+    currentDisplay.textContent = "";
+    lastDisplay.textContent = "";
+    currentOperand = "operand1";
+}
+
+function operationHelper(buttonText) {
+    if (operand1 === "-" || operand2 === "-" && operatorPressed) {
+        return;
+    }
+    else if (operand1 === "" && buttonText === "-") {
+        currentDisplay.textContent = "-";
+        operand1 += "-";
+        return;
+    }
+    else if (operand2 === "" && buttonText === "-" && operator !== "" && operand1 !== "-") {
+        currentDisplay.textContent = "-";
+        operand2 += "-";
+        return;
+    }
+    else if (operand1 === "") {
+        return;
+    }
+
+    if (!operatorPressed) {
+        operatorPressed = true;
+        operator = buttonText;
+        lastDisplay.textContent = `${operand1} ${operator}`;
+        currentOperand = "operand2";
+    }
+    else if (operand2 !== "" && operator !== "" && operatorPressed) {
+        result = operate(operand1, operand2, operator);
+        operator = buttonText;
+        lastDisplay.textContent = `${result} ${operator}`;
+        currentDisplay.textContent = `${result}`;
+        operand1 = result;
+        operand2 = "";
+    }
 }
 
 function displayHandler(event) {
@@ -52,69 +108,57 @@ function displayHandler(event) {
     }
 
     let buttonText = target.textContent;
-    
-    if (buttonText === "CLEAR") {
-        expression[0] = "";
-        expression[1] = "";
-        expression[2] = "";
-        currentDisplay.textContent = "";
-        lastDisplay.textContent = "";
+
+    if (buttonText === "+" || buttonText === "-" || buttonText === "x" || buttonText === "/") {
+        operationHelper(buttonText);
     }
-    else if (buttonText === "DELETE") {
-        if (expression[2] === "") {
-            expression[0] = expression[0].slice(0, -1);
-            currentDisplay.textContent = expression[0];
+    else if (buttonText === ".") {
+        if  (currentOperand === "operand1") {
+            if (!operand1.includes(".") && operand1 !== "") {
+                operand1 += ".";
+                currentDisplay.textContent = operand1;
+            }
         }
         else {
-            expression[2] = expression[2].slice(0, -1);
-            currentDisplay.textContent = expression[2];
+            if (!operand2.includes(".") && operand2 !== "") {
+                operand2 += ".";
+                currentDisplay.textContent = operand2;
+            }
         }
     }
     else if (buttonText === "=") {
-        if (expression[1] === "/" && expression[2] === "0") {
-            alert("You can't divide by 0!");
-            return;
-        }
-
-        if (expression[2] !== "") {
-            let result = operate(expression[0], expression[2], expression[1]);
-            currentDisplay.textContent = result;
-            lastDisplay.textContent = `${expression[0]} ${expression[1]} ${expression[2]} =`;
-            expression[0] = result + "";
-            expression[1] = "";
-            expression[2] = "";
+        if (operand1 !== "" && operand2 !== "" && operator !== "") {
+            result = operate(operand1, operand2, operator);
+            lastDisplay.textContent = `${operand1} ${operator} ${operand2} =`;
+            currentDisplay.textContent = `${result}`;
         }
     }
-    else if (!isNaN(buttonText)){
-        if (expression[2] === "" && expression[1] === "") {
-            expression[0] += buttonText;
-            currentDisplay.textContent = expression[0];
+    else if (buttonText === "CLEAR") {
+        clearCalculator();
+    }
+    else if (buttonText === "DELETE") {
+        if (currentOperand === "operand1") {
+            operand1 = operand1.slice(0, -1);
+            currentDisplay.textContent = operand1;
         }
-        else if (expression[1] !== "") {
-            expression[2] += buttonText;
-            currentDisplay.textContent = expression[2];
+        else {
+            operand2 = operand2.slice(0, -1);
+            currentDisplay.textContent = operand2;
         }
     }
     else {
-        if (expression[0] !== "" && expression[2] === "" && expression[1] === "-") {
-            expression[2] += buttonText;
-            currentDisplay.textContent = expression[2];
+        if (currentOperand === "operand1") {
+            operand1 += buttonText;
+            currentDisplay.textContent = operand1;
         }
-        else if (expression[0] !== "" && expression[2] === "") {
-            expression[1] = buttonText;
-            lastDisplay.textContent = `${expression[0]} ${expression[1]} ${expression[2]}`;
-        } 
-        else if (expression[1] === "-" && expression[2] === "") {
-            expression[2] += buttonText;
-            currentDisplay.textContent = expression[2];
-        }
-        else if (expression[0] === "" && buttonText === "-") {
-            expression[0] += buttonText;
-            currentDisplay.textContent = expression[0];
-        }
-        else if (expression[2] === "" && expression[0] !== "" && buttonText === "-") {
-            expression[2] += buttonText;
-            currentDisplay.textContent = expression[2];
+        else {
+            operand2 += buttonText;
+            if (operand2 === "0" && operator === "/") {
+                alert("You can't divide by 0!");
+                clearCalculator();
+                return;
+            }
+            currentDisplay.textContent = operand2;
         }
     }
 }
